8542db111196e0d891df4a40565753096e0da1c3
jcmore12 <42587685+jcmore12@users.noreply.github.com>
2018-08-28 13:55:41 -0500

    Add files via upload

---
+++ 02/sketch.js
@@ -0,0 +1,1060 @@
+function setup() {
+  createCanvas(350, 350);
+	background(255);
+
+   push();
+  //primera linea
+	var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+      var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+     //tercera linea
+     
+     //lineas inferiores
+     
+     
+     //cuarta linea
+     
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+    var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+     var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+  
+  //SEGUNDA LINEA
+ 
+  
+  push();
+ // triangulos inferiores
+  var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 30;
+    x2=x2+30;
+    x3=x3+30;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  
+    var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+  
+   { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  
+    var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+  
+   { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 70;
+    x2=x2+70;
+    x3=x3+70;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  //triangulos superiores
+  
+ var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+  var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+  
+    for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+  var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  pop();
+  
+  //tercera linea
+  push();
+  //triangulos inferiores
+  var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+   var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+     var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+       var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+  
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+  
+ // cuarta linea real
+  
+push();
+  //triangulos inferiores
+	var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+     var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 30;
+    x5=x5+30;
+    x6=x6+30;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+    var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+     var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 70;
+    x5=x5+70;
+    x6=x6+70;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+
+  //linea mitad
+  
+  push();
+  //triangulos inferiores
+	var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+     var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 30;
+    x5=x5+30;
+    x6=x6+30;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+    var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+     var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 70;
+    x5=x5+70;
+    x6=x6+70;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+  
+  
+  
+  
+  
+  //CUARTA LINEA
+  
+  //TRIANGULOS BASE INFERIORES
+
+  translate(0,40)
+  push();
+  //primera linea
+	var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+      var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 15;
+	var y1=15;
+	var x2= 15;
+	var y2=25;
+	var x3=25;
+	var y3=25;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+    var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+     var x4= 35
+  var y4= 15
+  var x5=45
+  var y5=15
+  var x6=45
+  var y6=25
+  
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+
+//sexta linea
+  
+  { translate(0,0)
+  push();
+ // triangulos inferiores
+  var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 30;
+    x2=x2+30;
+    x3=x3+30;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  
+    var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+  
+   { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  
+    var x1= 15;
+	var y1=25;
+	var x2= 25;
+	var y2=35;
+	var x3=15;
+	var y3=35;
+  
+   { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 70;
+    x2=x2+70;
+    x3=x3+70;
+     triangle(x1,y1,x2,y2,x3,y3);
+  
+    }
+  //triangulos superiores
+  
+ var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+  var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+  
+    for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+  var x4= 25
+  var y4= 25
+  var x5=35
+  var y5=25
+  var x6=35
+  var y6=35
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  pop();
+}
+  
+  //linea 7
+  
+  translate(0,0)
+   push();
+  //triangulos inferiores
+  var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+   var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 35;
+	var y1=35;
+	var x2= 45;
+	var y2=45;
+	var x3=35;
+	var y3=45;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 10;
+    x5=x5+10;
+    x6=x6+10;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+     var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+       var x4= 15
+  var y4= 35
+  var x5=25
+  var y5=35
+  var x6=25
+  var y6=45
+  
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 50;
+    x5=x5+50;
+    x6=x6+50;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+     
+  
+  
+  //ultima linea
+  
+  translate(0,0)
+  push();
+  //triangulos inferiores
+	var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 10;
+    x2=x2+10;
+    x3=x3+10;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+     
+     var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 40;
+    x2=x2+40;
+    x3=x3+40;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+   
+    var x1= 25;
+	var y1=45;
+	var x2= 35;
+	var y2=55;
+	var x3=25;
+	var y3=55;
+ 
+    { for(var i =0; i < 2; i = i + 1)
+  noStroke();
+	fill(0)
+  triangle(x1,y1,x2,y2,x3,y3);
+     
+    x1 = x1 + 50;
+    x2=x2+50;
+    x3=x3+50;
+     
+triangle(x1,y1,x2,y2,x3,y3);
+    }
+     
+
+     
+  //TRIANGULOS BASE SUPERIORES
+  var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 30;
+    x5=x5+30;
+    x6=x6+30;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  }
+  
+    var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  
+   for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 40;
+    x5=x5+40;
+    x6=x6+40;
+     
+	triangle(x4,y4,x5,y5,x6,y6); 
+  
+     var x4= 15
+  var y4= 45
+  var x5=25
+  var y5=45
+  var x6=25
+  var y6=55
+  { for(var i =0; i < 2; i = i + 1)
+	noStroke();
+	fill(0)
+	triangle(x4,y4,x5,y5,x6,y6); 
+      x4 = x4 + 70;
+    x5=x5+70;
+    x6=x6+70;
+   
+   	triangle(x4,y4,x5,y5,x6,y6);
+  }
+  pop();
+  
+}
